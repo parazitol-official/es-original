750c708942992b132fdf40a6591c2d2cf9d6a34a
parazitol-official <73265222+parazitol-official@users.noreply.github.com>
2020-10-27 09:25:01 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,255 @@
+var new_lang = {
+
+    "lt1": "GOLPE FUERTE A LOS PARÁSITOS",
+
+    "lt2": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Normaliza el funcionamiento del tracto grastro-intestinal",
+    
+    "lt3": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Elimina todas las especies de helmintos",
+    
+    "lt4": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Saca del organismo los productos de la actividad vital de los parásitos",
+    
+    "lt5": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Obstaculiza la repetición de la infección",
+    
+    "lt6": "PROPUESTA <br>ESPECIAL",
+    
+    "lt7": "hasta el final de la promoción falta ",
+    
+    "lt8": "<div class=\"price_old\"></div>  Precio común",
+    
+    "lt9": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: visible; animation-delay: 2s; animation-iteration-count: 3; animation-name: pulse;\"></div>  Precio de promoción",
+    
+    "lt10": "Ordenar",
+    
+    "lt11": "SEGÚN LOS ÚLTIMOS DATOS DE LA OMS,<br>MÁS DE MIL MILLONES DE PERSONAS ESTÁN INFECTADAS CON PARÁSITOS",
+    
+    "lt12": "El 90% de ellas ni siquiera se imaginan que se convirtieron en fuente de alimentación para los helmintos",
+    
+    "lt13": "¿USTED ESTÁ SEGURO QUE NO FORMA PARTE DE ESE NÚMERO? ¡VERIFIQUE QUE NO TIENE SIGNOS DE INFECCIÓN!",
+    
+    "lt14": "SIGNOS DE LA HELMINTIASIS",
+    
+    "lt15": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Sensación de cansancio",
+    
+    "lt16": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Dolores de cabeza, malestar",
+    
+    "lt17": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Infección de las vías respiratorias superiores",
+    
+    "lt18": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Irritabilidad, agresividad",
+    
+    "lt19": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Erupciones de todo tipo en la piel",
+    
+    "lt20": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Disminución de la masa corporal, incluso si tiene buen apetito",
+    
+    "lt21": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Problemas con el sueño",
+    
+    "lt22": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Alergia (lagrimeo, erupciones, rinorrea)",
+    
+    "lt23": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Resfriados frecuentes, angina, congestión nasal",
+    
+    "lt24": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Dolores en las articulaciones y los músculos",
+    
+    "lt25": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i>  Inflamación de los ganglios linfáticos",
+    
+    "lt26": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Trastornos digestivos: distensión abdominal, estreñimiento o diarreas",
+    
+    "lt27": "MEDIOS DE INFECCIÓN CON HELMINTOS",
+    
+    "lt28": "Vegetales, frutas y bayas mal lavadas",
+    
+    "lt29": "Carne y pescado poco cocidos",
+    
+    "lt30": "Agua o tierra infectada",
+    
+    "lt31": "Contacto con los animales domésticos",
+    
+    "lt32": "Lugares y transporte públicos",
+    
+    "lt33": "Picadas de insectos",
+    
+    "lt34": "Restaurantes y lugares de comida rápida",
+    
+    "lt35": "¡COMIENCE EL TRATAMIENTO YA!",
+    
+    "lt36": "COMENZAR",
+    
+    "lt37": "Parásitos <br>que provocan complicaciones horribles:",
+    
+    "lt38": "CISTICERCO",
+    
+    "lt39": "Lo más frecuente (más del 60%) es que el cisticerco llegue al cerebro, con menos frecuencia a los músculos esqueléticos y los ojos. Vive de 5 a 30 años, provoca <span>tumores cancerígenos</span> ",
+    
+    "lt40": "OXIUROS",
+    
+    "lt41": "Viven y se multiplican en los órganos del sistema digestivo. Conducen a la formación de <span>úlceras </span> y <span>hemorrágias generales</span> ",
+    
+    "lt42": "ANCYLOSTOMA",
+    
+    "lt43": "Dañan la mucosa y se alimentan de la sangre que emana debido a ello. Dejan pequeñas heridas sangrantes que conducen al desarrollo de <span>anemias graves </span> y <span>sepsis</span> ",
+    
+    "lt44": "ALVEOCOCO",
+    
+    "lt45": "Ocurre la muerte del tejido del órgano afectado. Además, se destruyen y <span>dejan de funcionar los vasos sanguíneos</span> ",
+    
+    "lt46": "EFECTO MULTIACTIVO <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+    
+    "lt47": "ELIMINA",
+    
+    "lt48": "todos los posibles tipos de invasiones en plazos muy cortos",
+    
+    "lt49": "EXPULSA",
+    
+    "lt50": "del organismo los productos de la actividad vital de los parásitos",
+    
+    "lt51": "CONTRIBUYE ",
+    
+    "lt52": "a la limpieza de las toxinas en el organismo",
+    
+    "lt53": "RENUEVA",
+    
+    "lt54": "al funcionamiento de los sistemas inmune, endocrino, digestivo, nervioso, cardiovascular",
+    
+    "lt55": "PROTEGE",
+    
+    "lt56": "de la repetición de la infección, forma un medio patógeno en el organismo humano y fortalece la inmunidad",
+    
+    "lt57": "ORDENAR",
+    
+    "lt58": "COMPOSICIÓN NATURAL POTENTE <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+    
+    "lt59": "MORINGA",
+    
+    "lt60": "A costa de su composición única, limpia el organismo de sustancias nocivas y metales pesados. Lucha de manera eficaz con los parásitos gastrointestinales, destruyendo los ADN ajenos. Limpia el intestino y las vías biliares.",
+    
+    "lt61": "PAU D'ARCO (LAPACHO)",
+    
+    "lt62": "El lapachol es un compuesto especial contenido en la corteza del lapacho, que obstruye la difusión de los microorganismos patógenos e inhibe la multiplicación y desarrollo de los parásitos intracelulares.",
+    
+    "lt63": "NOGAL NEGRO AMERICANO",
+    
+    "lt64": "Los elementos del nogal negro americano afectan el metabolismo de los gusanos, provocan la parálisis de los tejidos musculares de los parásitos, aumentan la inmunidad y las funciones defensivas del organismo, limpian la sangre de toxinas.",
+    
+    "lt65": "HOJAS DE OLIVO",
+    
+    "lt66": "Contiene oleuropeína, la cual posee efecto antiparasitario y antifúngico, disminuye el daño oxidativo en la sangre, el hígado, los riñones y los tejidos del cerebro.",
+    
+    "lt67": "RIZOMA DE CÚRCUMA",
+    
+    "lt68": "El rizoma de la cúrcuma contiene cineol, curcumina y la vitamina P. El cineol normaliza el trabajo de los órganos digestivos y afecta la actividad vital de los gusanos, la curcumina ejerce una acción antiparasitaria contra los nemátodos, la vitamina P restablece el funcionamiento de los órganos de hematopoyesis y limpia la sangre.",
+    
+    "lt69": "ASAFÉTIDA",
+    
+    "lt70": "Famosa por sus potentes propiedades bactericida y antimicrobiana. Se valora especialmente su capacidad para expulsar los gusanos acintados del tracto gastrointestinal.",
+    
+    "lt71": "¿POR QUÉ ES <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span>MEJOR QUE OTRAS PREPARACIONES?",
+    
+    "lt72": "PREPARACIONES QUÍMICAS",
+    
+    "lt74": "PREPARACIONES QUÍMICAS",
+    
+    "lt75": "Sustancias químicas<br> peligrosas en la composición <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt76": "Impacto en los órganos y sistemas internos<br> <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt77": "Contribuyen a la <br>disminución de la inmunidad <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt78": "Provocan daños en los órganos del tracto gastrointestinal: <br> <span>el hígado, el estómago, el páncreas, el intestino, provocan <br>gastritis y úlcera.</span> ",
+    
+    "lt80": "Total <br>composición natural <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt81": "No posee efectos colaterales<br>, protege los <br>órganos internos <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt82": "Aumenta la inmunidad<br> y la resistencia del <br>organismo a las infecciones <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+    
+    "lt83": "Ejerce una influencia beneficiosa sobre el tracto gastrointestinal, :<br> <span>mejora la secreción de la bilis, estimula el funcionamiento del intestino</span> ",
+    
+    "lt84": " <span>Parazitol</span> SUAVE PARA LAS PERSONAS - DURO PARA LOS HELMINTOS",
+    
+    "lt85": "«La mayoría de las personas ni siquiera sospechan que una de cada dos personas ESTÁ INFECTADO CON PARÁSITOS.",
+    
+    "lt86": "En el 95% de los casos, la presencia de helmintos no provoca ningún tipo de síntomas, hasta que ellos no conducen a un daño irreparable en los órganos internos y generen trastornos en el funcionamiento de todos los sistemas del organismo.",
+    
+    "lt87": "Al día de hoy, solamente la preparación Parazitol es capaz de limpiar TOTALMENTE el organismo de cualquier tipo de parásitos, de los cuales existen más de 250 especies. La preparación crea en el organismo de la persona un medio que destruye el propio proceso de parasitismo.",
+    
+    "lt88": "La experiencia de mis pacientes confirma que Parazitol es el medio más eficaz y seguro para la eliminación y expulsión de TODOS los tipos de invasiones, incluyendo los huevos, lo cual impide la repetición de la infección.",
+    
+    "lt89": "El efecto positivo se logra en el 100% de los casos».",
+    
+    "lt90": "Mijaíl González, médico-parasitólogo",
+    
+    "lt91": "EFECTO ADICIONAL <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+    
+    "lt92": "Ayuda a la limpieza de los órganos y tejidos, neutraliza la intoxicación como consecuencia de la infección parasitaria",
+    
+    "lt93": "Regenera las mucosas, limpia la sangre, la linfa, los líquidos intracelulares",
+    
+    "lt94": "Normaliza el equilibrio oxidación-reducción, que amenaza con la formación de células cancerígenas",
+    
+    "lt95": "Normaliza el equilibrio oxidación-reducción, que amenaza con la formación de células cancerígenas",
+    
+    "lt96": "Contribuye a liberarse de las úlceras y gastritis, matando las bacterias que las provocan",
+    
+    "lt97": "Restablece y normaliza las funciones de los órganos atacados",
+    
+    "lt98": "Influye de manera beneficiosa sobre los órganos del tracto gastrointestinal",
+    
+    "lt99": "Mejora el estado de la piel, el cabello, las uñas",
+    
+    "lt100": "ORDENAR",
+    
+    "lt101": "OPINIONES DE LOS CLIENTES",
+    
+    "lt102": "<div class=\"slide-description-title\">Elena, Barcelona </div> «A mí me gustó mucho este preparado. Yo y mi esposo lo tomamos. Los dos teníamos diferentes manifestaciones: sudor frío, heces líquidas, a mí me salieron granos y me dolía mucho la cabeza. Al cabo de una semana y media pasó la agudización ¡y ahora los dos nos sentimos perfectamente! Parazitol actúa suavemente, pero con fuerza y seguridad».",
+    
+    "lt103": "<div class=\"slide-description-title\">Susana, Valladolid </div> «Estuve sintiéndome mal durante dos semanas, me hice unos análisis, resultó que tenía parásitos. Tomé unas pastillas que me indicó el doctor, pero cada día me sentía peor. No sé que habría sido de mí si no me entero de la nueva preparación Parazitol. Fue lo que me salvó. Al cabo de un mes repetí los análisis y todo estaba limpio. Ahora tomaré Parazitol una vez al año como prevención».",
+    
+    "lt104": "<div class=\"slide-description-title\">Andrés, Getafe </div>«Nuestro gato se la pasa fuera de casa, le gusta pasear en el patio. Así fue como nos trajo los gusanos. Conmigo todo transcurrió bastante normal, pero mi esposa sufrió mucho. Al principio rechinaba los dientes por las noches, luego comenzó la fiebre y los vómitos. Nos tratamos juntos, porque era muy probable que ella me infectara a mí también. Tomamos Parazitol y todo finalmente pasó».",
+    
+    "lt105": "<div class=\"slide-description-title\">Elena, Barcelona </div> «A mí me gustó mucho este preparado. Yo y mi esposo lo tomamos. Los dos teníamos diferentes manifestaciones: sudor frío, heces líquidas, a mí me salieron granos y me dolía mucho la cabeza. Al cabo de una semana y media pasó la agudización ¡y ahora los dos nos sentimos perfectamente! Parazitol actúa suavemente, pero con fuerza y seguridad».",
+    
+    "lt106": "<div class=\"slide-description-title\">Susana, Valladolid </div> «Estuve sintiéndome mal durante dos semanas, me hice unos análisis, resultó que tenía parásitos. Tomé unas pastillas que me indicó el doctor, pero cada día me sentía peor. No sé que habría sido de mí si no me entero de la nueva preparación Parazitol. Fue lo que me salvó. Al cabo de un mes repetí los análisis y todo estaba limpio. Ahora tomaré Parazitol una vez al año como prevención».",
+    
+    "lt107": "<div class=\"slide-description-title\">Andrés, Bogotá </div> «Nuestro gato se la pasa fuera de casa, le gusta pasear en el patio. Así fue como nos trajo los gusanos. Conmigo todo transcurrió bastante normal, pero mi esposa sufrió mucho. Al principio rechinaba los dientes por las noches, luego comenzó la fiebre y los vómitos. Nos tratamos juntos, porque era muy probable que ella me infectara a mí también. Tomamos Parazitol y todo finalmente pasó».",
+    
+    "lt108": "<div class=\"slide-description-title\">Elena, Barcelona </div> «A mí me gustó mucho este preparado. Yo y mi esposo lo tomamos. Los dos teníamos diferentes manifestaciones: sudor frío, heces líquidas, a mí me salieron granos y me dolía mucho la cabeza. Al cabo de una semana y media pasó la agudización ¡y ahora los dos nos sentimos perfectamente! Parazitol actúa suavemente, pero con fuerza y seguridad».",
+    
+    "lt110": "GOLPE FUERTE A LOS PARÁSITOS",
+    
+    "lt111": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Normaliza el funcionamiento del tracto grastro-intestinal",
+    
+    "lt112": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Elimina todas las especies de helmintos",
+    
+    "lt113": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Saca del organismo los productos de la actividad vital de los parásitos",
+    
+    "lt114": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Obstaculiza la repetición de la infección",
+    
+    "lt115": "PROPUESTA <br>ESPECIAL",
+    
+    "lt116": "hasta el final de la promoción falta ",
+    
+    "lt117": "<div class=\"price_old\"></div>  Precio común",
+    
+    "lt118": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: hidden; animation-delay: 2s; animation-iteration-count: 3; animation-name: none;\"></div>  Precio de promoción",
+    
+    "lt119": "Ordenar",
+    
+    "lt120": "Cómo trabajamos",
+    
+    "lt121": "Haga su solicitud en nuestro sitio de internet",
+    
+    "lt122": "Un operador se comunicará con Usted para confirmar la orden",
+    
+    "lt123": "El pago se realiza al recibir el producto"
+    
+    
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
